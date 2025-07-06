2a9ca959e5d6e66ec4c1d8397c830d96f46d642a
Hemant <hemantmourya127@gmail.com>
2025-07-06 22:36:26 +0530

    picker

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,29 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default function Index() {
+  const [exercise, setExercise] = React.useState<string>("");
+  console.log("Current input:", exercise);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 24,
-          color: "green",
-        }}
-      >
-        Fitness Tracker- started(testing)
-      </Text>
+    <View style={styles.container}>
+      <TextInput style={styles.input} onChangeText={(newText) => setExercise(newText)} placeholder="Enter exercise" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 50,
+    width: 250,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+});
