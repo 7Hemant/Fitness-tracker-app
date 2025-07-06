1bf38f0453cba10335411d29178d05bbc4b4cda2
Hemant <hemantmourya127@gmail.com>
2025-07-06 21:42:54 +0530

    folder structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
           color: "green",
         }}
       >
-        Fitness Tracker- started
+        Fitness Tracker- started(testing)
       </Text>
     </View>
   );
